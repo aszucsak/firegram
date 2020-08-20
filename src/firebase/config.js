41223c1a1e64cdebcc5d2ba9677b0ba51b9626b1
András Szucsák <andras.szucsak@urss.hu>
2020-08-20 08:26:14 +0200

    set up firebase, start file upload

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+  import * as firebase from 'firebase/app';
+  import 'firebase/storage';
+  import 'firebase/firestore';
+
+  
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDkdwTjE9itlsvOL3E0O64yBFkNawBqPfw",
+    authDomain: "ninja-firegram-cff70.firebaseapp.com",
+    databaseURL: "https://ninja-firegram-cff70.firebaseio.com",
+    projectId: "ninja-firegram-cff70",
+    storageBucket: "ninja-firegram-cff70.appspot.com",
+    messagingSenderId: "582360945377",
+    appId: "1:582360945377:web:f8b924fb3918fda8545da0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.firestore();
+
+  export {projectFirestore, projectStorage};
